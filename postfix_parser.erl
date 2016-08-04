adad9a26821842d17bfe2b9426d4f224609be926
Eoin McCarthy <eoin@iliomad.com>
2016-08-04 14:40:35 +0100

    Somewhat dynamic loading of operator information.

--- postfix_parser.erl
+++ postfix_parser.erl
@@ -3,52 +3,52 @@
 
 tokeniseExpression(ExpStr) ->
     % Returns a list of tokens
+    % Erlang can't tell the difference between a list of integers and a string.
     string:tokens(ExpStr, " ").
 
-evaluateExpression([], Eval) ->
+evaluateExpression([], Eval, _) ->
     [Result] = Eval,
     Result;
-evaluateExpression(["0"|T], Eval) ->
-    evaluateExpression(T, [false | Eval]);
-evaluateExpression(["1"|T], Eval) ->
-    evaluateExpression(T, [true | Eval]);
-evaluateExpression([Operator|T], Eval) ->
+evaluateExpression(["0"|T], Eval, OpMap) ->
+    evaluateExpression(T, [false | Eval], OpMap);
+evaluateExpression(["1"|T], Eval, OpMap) ->
+    evaluateExpression(T, [true | Eval], OpMap);
+evaluateExpression([Operator|T], Eval, OpMap) ->
     % This is an operator
-    Result = applyOperator(Operator, Eval),
-    evaluateExpression(T, Result).
+    Result = applyOperator(Operator, Eval, OpMap),
+    evaluateExpression(T, Result, OpMap).
 
-applyOperator(Operator, Eval) ->
-    case Operator of 
-        "A" ->
-            [A,B|T] = Eval,
-            Result = A and B;
-        "R" ->
-            [A,B|T] = Eval,
-            Result = A or B;
-        "X" ->
-            [A,B|T] = Eval,
-            Result = A xor B;
-        "N" ->
-            [A|T] = Eval,
-            Result = not A
-    end,
-    [Result|T].
-            
+applyOperator(Operator, Eval, OpMap) ->
+    OperatorFun = maps:get(list_to_atom(Operator), OpMap),
+    {_,Arity} = erlang:fun_info(OperatorFun, arity),
+    {Args, Rest} = lists:split(Arity,Eval),
+    Result = erlang:apply(OperatorFun, Args),
+    [Result|Rest].
+
+operatorInfo() ->
+     #{
+        'A' => fun(A,B) -> A and B end,
+        'R' => fun(A,B) -> A or B end,
+        'X' => fun(A,B) -> A xor B end,
+        'N' => fun(A) -> not A end
+      }.
+
 
 parseExpression(ExpStr) ->
-    Result = evaluateExpression(tokeniseExpression(ExpStr), []),
-    io:format("~s: ~p\n", [ExpStr, Result]).
+    Result = evaluateExpression(tokeniseExpression(ExpStr), [], operatorInfo()),
+    io:format("~s: ~p\n", [ExpStr, Result]),
+    Result.
 
 
 runTestExpressions() ->
-    Tests = ["0 1 R",
-             "0 0 R",
-             "1 0 A 1 R N N",
-             "0 0 A 0 N 0 N A R",
-             "0 1 A 0 N 1 N A R",
-             "1 0 A 1 N 0 N A R",
-             "1 1 A 1 N 1 N A R",
-             "1 1 A 1 N 1 N A X",
-             "1 1 A 0 N 0 N A X"
+    Tests = [{"0 1 R", true},
+             {"0 0 R", false},
+             {"1 0 A 1 R N N", true},
+             {"0 0 A 0 N 0 N A R", true},
+             {"0 1 A 0 N 1 N A R", false},
+             {"1 0 A 1 N 0 N A R", false},
+             {"1 1 A 1 N 1 N A R", true},
+             {"1 1 A 1 N 1 N A X", true},
+             {"1 1 A 0 N 0 N A X", false}
             ],
-    [parseExpression(E) || E <- Tests].
+    [parseExpression(E) =:= Result || {E, Result} <- Tests].
